002e27c3caa0d0de64cbe696400fa87c82e6d5a4
Simone Azeglio <simone.azeglio@gmail.com>
2019-06-18 01:19:18 +0200

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,9 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon',
-              'sphinx.ext.viewcode', 'rst2pdf.pdfbuilder']
+              'sphinx.ext.viewcode']
 
-pdf_documents = [('index', u'MineNavigationDocumentation', u'MineNavigation', u'Simone Azeglio'),]
+#pdf_documents = [('index', u'MineNavigationDocumentation', u'MineNavigation', u'Simone Azeglio'),]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
